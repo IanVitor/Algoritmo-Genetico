257d4facaeaf035d99e111d7953482a5d9d69344
IanVitor <76691828+IanVitor@users.noreply.github.com>
2023-12-06 21:28:40 -0300

    update crossover

--- algoritmoGenetico.js
+++ algoritmoGenetico.js
@@ -7,7 +7,8 @@ Estrutura do Algoritmo
   - Inicialização da população - v
   - Avaliação dos indivíduos/Fitness - v
   - Seleção dos indivíduos
-  - Cross-over e Mutação
+  - Cross-over: Ponto de corte entre R e G, [R,*Ponto de corte* G, B]
+  - Mutação
   - Concepção da nova geração
 
 fitness
@@ -70,6 +71,56 @@ export default class AlgoritmoGenetico{
       indexPai++
     }
     return indexPai
+  }
+
+  crossOver(){
+    let filhos = []
+    let corte = parseInt((Math.random() * 1+1).toFixed(0))
+
+    let pai1 = this.selecionaPai(this.somaAvaliacoes())
+    let pai2 = this.selecionaPai(this.somaAvaliacoes())
+
+    let cromossomo1 = [this.populacao[pai1].cromossomo.red, this.populacao[pai1].cromossomo.green, this.populacao[pai1].cromossomo.blue]
+    let cromossomo2 = [this.populacao[pai1].cromossomo.red, this.populacao[pai1].cromossomo.green, this.populacao[pai1].cromossomo.blue]
+
+    console.log(corte)
+
+    /*
+      c1 [1,2,3] corte = 1
+      aux [5,6]
+
+      c2 [4,5,6]
+      c2 [4,2,3]
+      c1 [1,5,6]
+
+      mutação
+      0,2
+      0,134
+
+      [12,34,56]
+      [7,34,56]
+
+    */
+   let aux = []
+
+    aux.unshift(cromossomo2[0])
+    aux = cromossomo2.slice(corte)
+    cromossomo1.push(aux)
+
+
+    console.log(aux)
+    console.log(cromossomo2)
+    console.log(cromossomo1)
+
+    let filho1 = new Individuo({
+      red: 123,
+      green: 123,
+      blue: 123
+    })
+
+    filhos.push(new Individuo(randomRgb()))
+
+    return filhos
   }
 
   imprimirGeracao(){

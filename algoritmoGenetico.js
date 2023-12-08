02b89455db35fe40515ea6b7896a69c6247e8f75
IanVitor <76691828+IanVitor@users.noreply.github.com>
2023-12-07 22:32:57 -0300

    docs

--- algoritmoGenetico.js
+++ algoritmoGenetico.js
@@ -19,110 +19,108 @@ fitness
 
 */
 
-import Individuo from './Individuo.js'
+import Individuo from "./Individuo.js";
 
-export default class AlgoritmoGenetico{
-  constructor(tamanhoPopulacao){
-    this.tamanhoPopulacao = tamanhoPopulacao
-    this.populacao = []
-    this.melhorPopulacao = []
+export default class AlgoritmoGenetico {
+  constructor(tamanhoPopulacao) {
+    this.tamanhoPopulacao = tamanhoPopulacao;
   }
 
-  inicializarPopulacao() {
-    this.populacao = []
-    for(let i =0; i<this.tamanhoPopulacao; i++){
-      this.populacao.push(new Individuo(randomRgb()))
+  inicializarPopulacao(arrayPopulacao) {
+    
+    for (let i = 0; i < this.tamanhoPopulacao; i++) {
+      arrayPopulacao.push(new Individuo(randomRgb()));
     }
   }
 
-  calcularFitness(){
-    for(let i =0; i<this.tamanhoPopulacao; i++){
-      this.populacao[i].avaliacao()
+  calcularFitness(arrayPopulacao) {
+    for (let i = 0; i < arrayPopulacao.length; i++) {
+      arrayPopulacao[i].avaliacao();
     }
   }
 
-  avaliarIndividuos(){
-    this.populacao.sort((a, b) =>{
-      if(a.fitness > b.fitness)
-        return -1
-      if(a.fitness < b.fitness)
-        return 1
+  avaliarIndividuos(arrayPopulacao) {
+    arrayPopulacao.sort((a, b) => {
+      if (a.fitness > b.fitness) return -1;
+      if (a.fitness < b.fitness) return 1;
 
-      return 0
-    })
-
-    console.log(this.populacao)
+      return 0;
+    });
   }
 
-  somaAvaliacoes(){
-    let soma = 0
-    for(let i=0; i< this.populacao.length; i++){
-      soma += parseFloat(this.populacao[i].fitness)
+  somaAvaliacoes(arrayPopulacao) {
+    let soma = 0;
+    for (let i = 0; i < arrayPopulacao.length; i++) {
+      soma += parseFloat(arrayPopulacao[i].fitness);
     }
-    return soma
+    return soma;
   }
 
-  selecionaPai(somaAvaliacoes){
-    let valorSorteado = Math.random()*somaAvaliacoes
-    let i = 0, indexPai = -1, soma = 0
-    while(i< this.populacao.length && soma < valorSorteado){
-      soma += parseFloat(this.populacao[i].fitness)
-      i++
-      indexPai++
+  selecionaPai(somaAvaliacoes, arrayPopulacao) {
+    let valorSorteado = Math.random() * somaAvaliacoes;
+    let i = 0,
+      indexPai = -1,
+      soma = 0;
+    while (i < arrayPopulacao.length && soma < valorSorteado) {
+      soma += parseFloat(arrayPopulacao[i].fitness);
+      i++;
+      indexPai++;
     }
-    return indexPai
+    return indexPai;
   }
 
-  crossOver(){
-    let corte = parseInt((Math.random() * 1+1).toFixed(0))
-    let filhos = []
+  crossOver(arrayPopulacao, novaPopulacao) {
+    let corte = parseInt((Math.random() * 1 + 1).toFixed(0));
 
-    let pai1 = this.selecionaPai(this.somaAvaliacoes())
-    let pai2 = this.selecionaPai(this.somaAvaliacoes())
+    let pai1 = this.selecionaPai(this.somaAvaliacoes(arrayPopulacao),arrayPopulacao);
+    let pai2 = this.selecionaPai(this.somaAvaliacoes(arrayPopulacao),arrayPopulacao);
 
-    let cromossomo1 = [this.populacao[pai1].cromossomo.red, this.populacao[pai1].cromossomo.green, this.populacao[pai1].cromossomo.blue]
-    let cromossomo2 = [this.populacao[pai2].cromossomo.red, this.populacao[pai2].cromossomo.green, this.populacao[pai2].cromossomo.blue]
+    let cromossomo1 = [
+      arrayPopulacao[pai1].cromossomo.red,
+      arrayPopulacao[pai1].cromossomo.green,
+      arrayPopulacao[pai1].cromossomo.blue,
+    ];
+    let cromossomo2 = [
+      arrayPopulacao[pai2].cromossomo.red,
+      arrayPopulacao[pai2].cromossomo.green,
+      arrayPopulacao[pai2].cromossomo.blue,
+    ];
 
-    console.log('----Teste de Crossover----')
+    if (corte === 1) {
+      let teste = cromossomo1[0];
+      cromossomo1[0] = cromossomo2[0];
+      cromossomo2[0] = teste;
+    } else {
+      let teste = [];
+      teste[0] = cromossomo1[0];
+      teste[1] = cromossomo1[1];
+      cromossomo1[0] = cromossomo2[0];
+      cromossomo1[1] = cromossomo2[1];
+      cromossomo2[0] = teste[0];
+      cromossomo2[1] = teste[1];
+    }
 
-    console.log('Posição do corte: '+corte)
-    console.log('Cromossomo 1: '+cromossomo1)
-    console.log('Cromossomo 2: '+cromossomo2)
+    let cromossomoFilho1 = {
+      red: cromossomo1[0],
+      green: cromossomo1[1],
+      blue: cromossomo1[2],
+    }
 
-    let aux = cromossomo2.slice(0,corte)
+    novaPopulacao.push(new Individuo(cromossomoFilho1))
 
-    console.log('aux: '+aux)
+    let cromossomoFilho2 = {
+        red: cromossomo2[0],
+        green: cromossomo2[1],
+        blue: cromossomo2[2]
+    }  
 
-    console.log('cromossomo 2: '+cromossomo2.slice(0,corte))
-    console.log('cromossomo teste: '+cromossomo1.slice(corte,3))
-
-    console.log('\ncromossomo 1: '+cromossomo1.slice(0,corte))
-    console.log('cromossomo teste: '+cromossomo2.slice(corte,3))
-
-    aux.push(cromossomo1.slice(corte,3))
-    console.log('filho 2: '+aux)
-    console.log('filho: '+(aux.splice(1)).shift())
-
-    filhos.push({
-      cromossomo: {
-        red: aux[0],
-        green: aux[1],
-        blue: aux[2],
-      },
-      geracao: this.populacao[0].geracao+1,
-      fitness: 0.5
-    })
-
-    aux = cromossomo1.slice(0,corte)
-    aux.push(cromossomo2.slice(corte,3))
-    console.log('filho 1: '+aux)
-
-
-    return filhos
+    novaPopulacao.push(new Individuo(cromossomoFilho2))
   }
 
-  imprimirGeracao(){
-    console.log(`Melhor da Geração\nGeração: ${this.populacao[0].geracao}\nFitness: ${this.populacao[0].fitness}\nCromossomo: [${this.populacao[0].cromossomo.red},${this.populacao[0].cromossomo.green},${this.populacao[0].cromossomo.blue}]`)
+  imprimirGeracao(arrayPopulacao) {
+    console.log(
+      `Melhor da Geração\nGeração: ${arrayPopulacao[0].geracao}\nFitness: ${arrayPopulacao[0].fitness}\nCromossomo: [${arrayPopulacao[0].cromossomo.red},${arrayPopulacao[0].cromossomo.green},${arrayPopulacao[0].cromossomo.blue}]`
+    );
   }
 }
 
@@ -130,8 +128,8 @@ const randomRgb = () => {
   let cromossomo = {
     red: parseInt((Math.random() * 255).toFixed(0)),
     green: parseInt((Math.random() * 255).toFixed(0)),
-    blue: parseInt((Math.random() * 255).toFixed(0))
-  }
+    blue: parseInt((Math.random() * 255).toFixed(0)),
+  };
 
-  return cromossomo
-}
+  return cromossomo;
+};
